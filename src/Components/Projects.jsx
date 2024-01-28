8b795ccb33085a9f078f8c9e429e9778b64c5822
Jasmeet Singh <jasmeet5108k@gmail.com>
2024-01-28 16:27:25 +0530

    Added a new project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
             <section id='projects' className='px-6 md:mt-8 max-w-screen-xl mx-auto md:flex md:flex-col md:items-center'>
 
                 <div id='Calculator' className='border lg:w-[900px] lg:h-[300px] border-gray-500 flex flex-col md:flex-row-reverse md:justify-between p-4 mt-4 rounded-md'>
-                    <img src="Images/calculator-Img.jpg" alt="" className='rounded-md w-full md:w-[350px]' />
+                    <img src="Images/calculator-Img.jpg" alt="Calculator Image" className='rounded-md w-full md:w-[350px]' />
                     <div className='md:flex md:flex-col md:space-y-4 md:justify-center'>
                         <p className='mt-4 text-2xl text-center md:text-left font-semibold blue-gradient_text'>Simple Calculator</p>
                         <p className='text-lg md:text-lg md:text-left md:w-[350px] mt-4 text-center'>A simple calculator app made using HTML, CSS, Javascript. Deployed on Netlify</p>
@@ -19,13 +19,25 @@ const Projects = () => {
                 </div>
 
                 <div id="Hosting" className='mt-12 border lg:w-[900px] lg:h-[300px] border-gray-500 flex flex-col md:flex-row-reverse md:justify-between p-4 rounded-md'>
-                    <img src="Images/Web-Hosting-Img.jpg" alt="" className='rounded-md w-full border md:w-[350px]' />
+                    <img src="Images/Web-Hosting-Img.jpg" alt="Web Hosting Image" className='rounded-md w-full border md:w-[350px]' />
                     <div className='md:flex md:flex-col md:space-y-4 md:justify-center'>
                         <p className='mt-4 text-2xl text-center md:text-left font-semibold blue-gradient_text'>Web Hosting</p>
                         <p className='text-lg mt-4 text-center md:pr-2 md:w-[350px] md:text-left'>A personal project regarding web Hosting made using HTML, CSS, Javascript. Deployed on Vercel</p>
                         <div id="buttons" className='mt-5 flex justify-center md:justify-start space-x-5'>
                             <a href='https://github.com/Jasmeet5108/Web-Hosting-Project' target='_blank' className='bg-gradient-to-r from-[#00c6ff] to-[#0072ff] hover:from-[#0072ff] hover:to-[#00c6ff] transition text-white rounded p-2 hover:cursor-pointer'>Code on Github</a>
                             <a href='https://web-hosting-project.vercel.app' target='_blank' className='bg-gradient-to-r from-[#00c6ff] to-[#0072ff] hover:from-[#0072ff] hover:to-[#00c6ff] transition text-white rounded p-2 hover:cursor-pointer'>See live version</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="Movie Streaming" className='mt-12 border lg:w-[900px] lg:h-[300px] border-gray-500 flex flex-col md:flex-row-reverse md:justify-between p-4 rounded-md'>
+                    <img src="Images/Movie-Streaming-Img.jpg" alt="Movie Image" className='rounded-md w-full border md:w-[350px]' />
+                    <div className='md:flex md:flex-col md:space-y-4 md:justify-center'>
+                        <p className='mt-4 text-2xl text-center md:text-left font-semibold blue-gradient_text'>Movie Streaming App</p>
+                        <p className='text-lg mt-4 text-center md:pr-2 md:w-[350px] md:text-left'>A movie straming app made using Reactjs and TailwindCSS. Deployed on Vercel</p>
+                        <div id="buttons" className='mt-5 flex justify-center md:justify-start space-x-5'>
+                            <a href='https://github.com/Jasmeet5108/Movie-Streaming-App' target='_blank' className='bg-gradient-to-r from-[#00c6ff] to-[#0072ff] hover:from-[#0072ff] hover:to-[#00c6ff] transition text-white rounded p-2 hover:cursor-pointer'>Code on Github</a>
+                            <a href='https://movie-streaming-app-ten.vercel.app' target='_blank' className='bg-gradient-to-r from-[#00c6ff] to-[#0072ff] hover:from-[#0072ff] hover:to-[#00c6ff] transition text-white rounded p-2 hover:cursor-pointer'>See live version</a>
                         </div>
                     </div>
                 </div>
